4545d954a065d9a2b27edaff7cb3cfde8e038eb6
Katamaranas <ernestas.zidokas@gmail.com>
2019-03-25 14:40:50 +0200

    forma + class

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,69 @@
 <?php
 require '../bootloader.php';
 
+$form = [
+    'fields' => [
+        'drink_name' => [
+            'label' => 'Drink name',
+            'type' => 'text',
+            'placeholder' => 'Alkoholis 3000',
+            'validate' => [
+                'validate_not_empty',
+            ]
+        ],
+        'drink_abarot' => [
+            'label' => 'Drink abarot',
+            'type' => 'float',
+            'placeholder' => '5.0',
+            'validate' => [
+                'validate_not_empty',
+            ]
+        ],
+        'drink_amount' => [
+            'label' => 'Drink amount',
+            'type' => 'number',
+            'placeholder' => '500',
+            'validate' => [
+                'validate_not_empty',
+            ]
+        ],
+        'drink_foto' => [
+            'label' => 'Drink foto',
+            'placeholder' => 'file',
+            'type' => 'file',
+            'validate' => [
+                'validate_file'
+            ]
+        ]
+    ],
+    'buttons' => [
+        'submit' => [
+            'text' => 'Paduoti!'
+        ]
+    ],
+    'callbacks' => [
+        'success' => [
+            'form_success'
+        ],
+        'fail' => []
+    ]
+];
+
+function form_success($safe_input, $form) {
+    $file_saved_url = save_file($safe_input['drink_foto']);
+    var_dump($file_saved_url);
+    $gerimas = new \App\Item\Gerimas([
+        'name' => $safe_input['drink_name'],
+        'amount_ml' => $safe_input['drink_abarot'],
+        'abarot' => $safe_input['drink_amount'],
+        'image' => $file_saved_url
+    ]);
+    var_dump($safe_input);
+    $db = new Core\FileDB(ROOT_DIR . '/app/files/db.txt');
+    $model_gerimai = new App\model\ModelGerimai($db, 'input_kokteiliai');
+    $model_gerimai->insert(date("Y-m-d-H:m:s"), $gerimas);
+}
+
 $db = new Core\FileDB(ROOT_DIR . '/app/files/db.txt');
 $db->save();
 
@@ -37,6 +100,33 @@ $model_gerimai->insert('vodka', $vodka);
 $model_gerimai->insert('vanduo', $vanduo);
 
 //$model_gerimai->deleteRows();
+
+function save_file($file, $dir = 'uploads', $allowed_types = ['image/png', 'image/jpg']) {
+    var_dump('save file iskviestas');
+    var_dump($file);
+    if ($file['error'] == 0 && in_array($file['type'], $allowed_types)) {
+        var_dump('type is ok');
+        $target_file_name = microtime() . '-' . strtolower($file['name']);
+        $target_path = $dir . '/' . $target_file_name;
+        if (move_uploaded_file($file['tmp_name'], $target_path)) {
+            var_dump('file moved');
+            return $target_file_name;
+        }
+    }
+    return false;
+}
+
+$success_msg = '';
+
+if (!empty($_POST)) {
+    $safe_input = get_safe_input($form);
+    $form_success = validate_form($safe_input, $form);
+    if ($form_success) {
+        $success_msg = strtr('Gerimas "@drink_name" sėkmingai sukurtas!', [
+            '@drink_name' => $safe_input['drink_name']
+        ]);
+    }
+}
 ?>
 <html>
     <head>
@@ -51,5 +141,7 @@ $model_gerimai->insert('vanduo', $vanduo);
                 <img src="<?php print $gerimas->getImage(); ?>"
             </div>
         <?php endforeach; ?>
+        <?php require '../core/views/form.php'; ?>
+        <h3><?php print $success_msg; ?></h3>
     </body>
 </html>
