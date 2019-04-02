a794bc40a51b07ba71806c8fec70d22d77733af3
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-02 13:29:30 +0300

    boyfriend class

---
+++ app/classes/Boyfriend.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class Boyfriend extends Boy {
+
+    public function winArgument() {        
+        $array = ['Not this time!', parent::winArgument()]; 
+        
+        return $array[rand(0,1)];
+    }
+
+}
