c7f05a267d050c4310ac8351774b4fc8627c3754
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-02 12:35:32 +0300

    horriblewife

---
+++ app/classes/Horriblewife.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class HorribleWife extends Girlfriend {
+
+    public function pistiProta() {
+        return parent::pistiProta() . 'Visiems TAIP buna...tu ne pirmas...';
+    }
+
+}
