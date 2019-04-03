2233edf5955630dc1fa2655cd8111c40ce387ea3
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-03 13:30:05 +0300

    html readings'

--- app/classes/SensorFartHumidity.php
+++ app/classes/SensorFartHumidity.php
@@ -5,7 +5,6 @@ namespace App;
 class SensorFartHumidity extends Abstracts\Sensor {
 
     public function read() {
-        
         return $this->reading = rand(0, 100) . '%';
     }
 
--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,21 @@
 <?php
 require_once '../bootloader.php';
 
+$sensors = new \App\Sensors();
+$fart_sensor = new App\SensorFartTemp;
+$humidity_sensor = new App\SensorFartHumidity;
+$sensors->add('Temp Sensor', $fart_sensor);
+$sensors->add('Humidity Sensor', $humidity_sensor);
 ?>
+<html>
+    <head>
+        <title>Sensors</title>
+    </head>
+    <body>
+        <div>
+            <?php foreach ($sensors->getReadings() as $id => $reading): ?>
+                <p><?php print $id . ': ' . $reading; ?></p>
+            <?php endforeach; ?>
+        </div>
+    </body>
+</html>
