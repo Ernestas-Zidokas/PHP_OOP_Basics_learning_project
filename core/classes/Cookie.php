45fec42e654e46d2b0f66e723b86013c2ada3d4f
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-04 13:32:22 +0300

    cookie save

--- core/classes/Cookie.php
+++ core/classes/Cookie.php
@@ -29,23 +29,7 @@ class Cookie extends Core\Abstracts\Cookie {
         return [];
     }
 
-    /**
-     * Turi į Cookie duotu pavadinimu
-     * išsaugoti json_encode'intą $data array'jų
-     * (Google setcookie)
-     * 
-     * Į cookie galima įrašyt tik string'ą.
-     * Kadangi mes norim galimybę turėti į tą patį
-     * Cookie storinti daugiau data'os, galim tiesiog
-     * encode'inti ir decode'inti array'jų su json'u.
-     * 
-     * Mes į cookie įrašysim už'json_encodinę $data
-     * ir atkursim atgal json_decode'inę tai ką radom Cookie
-     * 
-     * @param $data array
-     * @param $expires_in int Už kiek laiko sekundemis cookie nebegalios
-     */
-    public function save($data, $expires_in = 3600): void {
+    public function save(array $data, int $expires_in = 3600): void {
         setcookie($this->name, json_encode($data), time() + $expires_in);
     }
 
